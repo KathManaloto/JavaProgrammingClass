000d060dc4006123deac91bd60732ca94d6bfbbd
Kathlene Manaloto <kmanaloto.github@gmail.com>
2025-08-29 14:07:13 +0000

    Done TaskSheet 1-1-1

---
+++ TaskSheet111.java
@@ -0,0 +1,13 @@
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+
+package chapter1;
+
+class HelloWorldApp {
+
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // Display the string.
+    }
+}
